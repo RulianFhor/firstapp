bda9678dda1269310cd7716717a44cdcce8173ca
RulianFhor <julian.horlaville@gmail.com>
2018-05-04 16:54:33 -0400

    basic polls working

--- polls/urls.py
+++ polls/urls.py
@@ -4,13 +4,12 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
     # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('spec/<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('spec/<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/spec/5/
+    path('spec/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/spec/5/results/
+    path('spec/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/spec/5/vote/
     path('spec/<int:question_id>/vote/', views.vote, name='vote'),
 ]
